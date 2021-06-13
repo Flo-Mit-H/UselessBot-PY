d59bc31016e025eba095ac48353f7661bf17fb55
Flo Mit H <Flogamesg@gmail.com>
2021-06-13 19:53:25 +0200

    - Console Error Fixes

--- minecraft.py
+++ minecraft.py
@@ -13,18 +13,22 @@ class Minecraft(commands.Cog):
 
     @commands.Cog.listener()
     async def on_message(self, message):
-        if not message.channel.id == main.config["console-channel"]:
+        if not message.channel.id == main.config["minecraft"]["console-channel"]:
             return
         if message.author.bot:
             return
-        if message.content.startswith(main.config["prefix"]):
-            command = message.content[len(main.config["prefix"]):]
-            with mcrcon.MCRcon(host=main.config["server-host"], password=main.config["server-password"], port=main.config["server-port"]) as server:
-                server.connect()
-                feedback = server.command(command)
-                for char in self.invalid_characters:
-                    feedback = feedback.replace(char, "")
-                await message.channel.send(f"```{feedback}```")
+        if message.content.startswith(main.config["minecraft"]["prefix"]):
+            command = message.content[len(main.config["minecraft"]["prefix"]):]
+            try:
+                with mcrcon.MCRcon(host=main.config["minecraft"]["server-host"], password=main.config["minecraft"]["server-password"], port=main.config["minecraft"]["server-port"]) as server:
+                    server.connect()
+                    feedback = server.command(command)
+                    for char in self.invalid_characters:
+                        feedback = feedback.replace(char, "")
+                    await message.channel.send(f"```{feedback}```")
+            except ConnectionRefusedError:
+                await message.channel.send(f"Verbindung zum Server {main.config['minecraft']['server-host']}:{main.config['minecraft']['server-port']} "
+                                           f"with password {main.config['minecraft']['server-password']} fehlgeschlagen")
 
 
 def setup(bot):
